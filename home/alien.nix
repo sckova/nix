b3934cfd7d415c9f659af175799c294337c390c3
Sean Kovacs <kovacsmillio@gmail.com>
2025-11-07 18:42:22 -0500

    add kate and alien global menu

--- home/alien.nix
+++ home/alien.nix
@@ -19,5 +19,40 @@
         DefaultLightLookAndFeel = "Catppuccin-Latte-Blue";
       };
     };
+    panels = [
+      # Alternative global menu to fit the modified taskbar
+      {
+        location = "bottom";
+        height = 40;
+        hiding = "dodgewindows";
+        lengthMode = "fit";
+        floating = true;
+        alignment = "right";
+        widgets = [
+          "org.kde.plasma.appmenu"
+          {
+            plasmusicToolbar = {
+              panelIcon = {
+                albumCover = {
+                  useAsIcon = false;
+                  radius = 8;
+                };
+                icon = "view-media-track";
+              };
+              playbackSource = "auto";
+              musicControls.showPlaybackControls = true;
+              songText = {
+                displayInSeparateLines = true;
+                maximumWidth = 640;
+                scrolling = {
+                  behavior = "alwaysScroll";
+                  speed = 3;
+                };
+              };
+            };
+          }
+        ];
+      }
+    ];
   };
 }
