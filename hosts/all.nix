28f3f82796b1a58521929c6b8837d156c2d19210
Sean Kovacs <kovacsmillio@gmail.com>
2025-11-08 09:52:26 -0500

    add strawberry from git

--- hosts/all.nix
+++ hosts/all.nix
@@ -211,6 +211,10 @@
   services.openssh.enable = true;
   networking.firewall.enable = false;
 
+  networking.extraHosts = ''
+    0.0.0.0 apresolve.spotify.com
+  '';
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
