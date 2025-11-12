bdac0a9eac78c6b63613c0492e878dc95b9761bd
Sean Kovacs <kovacsmillio@gmail.com>
2025-11-12 13:30:49 -0500

    add synology mount

--- home/all.nix
+++ home/all.nix
@@ -25,6 +25,7 @@
     input-leap
     libreoffice-qt-fresh
     spotdl
+    rclone
 
     kde-rounded-corners
     kdePackages.partitionmanager
@@ -50,6 +51,32 @@
     };
     firefox = {
       force = true;
+    };
+  };
+
+  xdg.configFile."rclone/synology.conf".text = ''
+    [synology]
+    type = sftp
+    user = sckova
+    host = nas.taila30609.ts.net
+    key_file = ~/.ssh/key
+  '';
+
+  systemd.user.services.synology-mount = {
+    Unit = {
+      Description = "Mount Synology NAS with Rclone and Home Manager.";
+      After = [ "network-online.target" ];
+    };
+
+    Service = {
+      Type = "notify";
+      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p %h/Synology";
+      ExecStart = "${pkgs.rclone}/bin/rclone --config=%h/.config/rclone/synology.conf --vfs-cache-mode full --ignore-checksum mount \"synology:\" \"%h/Synology\"";
+      ExecStop = "/run/wrappers/bin/fusermount -u %h/Synology/%i";
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
     };
   };
 
