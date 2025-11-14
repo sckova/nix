24961c4333d5419473bb20ab4f4789cbbbb25513
Sean Kovacs <kovacsmillio@gmail.com>
2025-11-13 21:36:37 -0500

    add btop and update kitty

--- home/terminal/kitty/default.nix
+++ home/terminal/kitty/default.nix
@@ -10,6 +10,13 @@
     shellIntegration = {
       enableFishIntegration = true;
     };
-    extraConfig = "\nwheel_scroll_multiplier 5.0";
+    # extraConfig = "\nwheel_scroll_multiplier 5.0\nconfirm_os_window_close 0";
+    settings = {
+      scrollback_lines = 10000;
+      enable_audio_bell = false;
+      update_check_interval = 0;
+      wheel_scroll_multiplier = 5.0;
+      confirm_os_window_close = 0;
+    };
   };
 }
