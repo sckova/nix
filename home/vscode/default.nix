2fd35967386f780ea7bce9093ee07ba60aac59f7
Sean Kovacs <kovacsmillio@gmail.com>
2025-11-19 14:36:10 -0500

    update vscode

--- home/vscode/default.nix
+++ home/vscode/default.nix
@@ -1,26 +1,50 @@
-{ pkgs, ... }:
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  capitalize =
+    str:
+    (lib.toUpper (builtins.substring 0 1 str)) + (builtins.substring 1 (builtins.stringLength str) str);
+in
+{
+  catppuccin.vscode.profiles.default = {
+    enable = true;
+    settings = {
+      workbenchMode = "minimal";
+    };
+  };
+
   programs.vscode = {
     enable = true;
     profiles.default = {
       enableExtensionUpdateCheck = true;
       enableMcpIntegration = true;
       enableUpdateCheck = true;
+      # https://search.nixos.org/packages?query=vscode-extensions
       extensions = with pkgs.vscode-extensions; [
+        yzhang.markdown-all-in-one
+        jnoortheen.nix-ide
+        esbenp.prettier-vscode
       ];
       userSettings = {
-        "workbench.colorTheme" = "Catppuccin Alt Mocha";
         "window.menuBarVisibility" = "compact";
         "workbench.navigationControl.enabled" = false;
         "window.commandCenter" = false;
-        "chat.commandCenter.enabled" = false;
         "workbench.layoutControl.enabled" = false;
         "window.titleBarStyle" = "native";
         "editor.minimap.enabled" = false;
-        "chat.mcp.gallery.enabled" = true;
-
         "editor.semanticHighlighting.enabled" = true;
-        "workbench.iconTheme" = "catppuccin-mocha";
+
+        # Disable AI "features"
+        ## Chat features
+        "chat.agent.enabled" = false;
+        "chat.commandCenter.enabled" = false;
+        "inlineChat.accessibleDiffView" = "off";
+        "terminal.integrated.initialHint" = false;
       };
     };
   };
