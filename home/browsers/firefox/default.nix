817a865e399151906e246ebbaa3f91143a88b251
Sean Kovacs <kovacsmillio@gmail.com>
2025-12-03 11:04:34 -0500

    changes

--- home/browsers/firefox/default.nix
+++ home/browsers/firefox/default.nix
@@ -27,7 +27,7 @@
       # "force_installed" and "normal_installed".
       ExtensionSettings = {
         "*" = {
-          "installation_mode" = "blocked";
+          "installation_mode" = "allowed";
         };
         "uBlock0@raymondhill.net" = {
           installation_mode = "allowed";
@@ -167,6 +167,27 @@
             "fxa-toolbar-menu-button"
             "unified-extensions-button"
           ];
+
+          # Undo a bunch of stuff that LibreWolf does
+          # "browser.safebrowsing.malware.enabled" = true;
+          # "browser.safebrowsing.phishing.enabled" = true;
+          # "browser.safebrowsing.blockedURIs.enabled" = true;
+          # "browser.safebrowsing.provider.google4.gethashURL" =
+          #   "https://safebrowsing.googleapis.com/v4/fullHashes:find?$ct=application/x-protobuf&key=%GOOGLE_SAFEBROWSING_API_KEY%&$httpMethod=POST";
+          # "browser.safebrowsing.provider.google4.updateURL" =
+          #   "https://safebrowsing.googleapis.com/v4/threatListUpdates:fetch?$ct=application/x-protobuf&key=%GOOGLE_SAFEBROWSING_API_KEY%&$httpMethod=POST";
+          # "browser.safebrowsing.provider.google.gethashURL" =
+          #   "https://safebrowsing.google.com/safebrowsing/gethash?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2";
+          # "browser.safebrowsing.provider.google.updateURL" =
+          #   "https://safebrowsing.google.com/safebrowsing/downloads?client=SAFEBROWSING_ID&appver=%MAJOR_VERSION%&pver=2.2&key=%GOOGLE_SAFEBROWSING_API_KEY%";
+          # "browser.safebrowsing.downloads.enabled" = true;
+          # "privacy.resistFingerprinting.letterboxing" = false;
+          # "webgl.disabled" = false;
+          # "identity.fxaccounts.enabled" = true;
+          # "privacy.clearOnShutdown.history" = false;
+          # "privacy.clearOnShutdown.downloads" = false;
+          # "privacy.fingerprintingProtection" = false;
+          # "network.cookie.lifetimePolicy" = 0;
         };
         bookmarks = {
           force = true;
@@ -178,16 +199,6 @@
                 {
                   name = "NixOS";
                   url = "https://nixos.org/";
-                }
-              ];
-            }
-            {
-              name = "ovips.us.to";
-              toolbar = true;
-              bookmarks = [
-                {
-                  name = "ovips.us.to";
-                  url = "https://ovips.us.to/";
                 }
               ];
             }
