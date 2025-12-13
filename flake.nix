156dc923694c3e180c31d15625a52abd9c2fb1fb
Sean Kovacs <kovacsmillio@gmail.com>
2025-12-13 13:09:33 -0500

    if it ain't broke

--- flake.nix
+++ flake.nix
@@ -2,36 +2,36 @@
   description = "My NixOS Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/8bb5646e0bed5dbd3ab08c7a7cc15b75ab4e1d0f";
 
     catppuccin = {
-      url = "github:catppuccin/nix";
+      url = "github:catppuccin/nix/a696fed6b9b6aa89ef495842cdca3fc2a7cef0de";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/e1680d594a9281651cbf7d126941a8c8e2396183";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     plasma-manager = {
-      url = "github:nix-community/plasma-manager";
+      url = "github:nix-community/plasma-manager/b24ed4b272256dfc1cc2291f89a9821d5f9e14b4";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
 
     nur = {
-      url = "github:nix-community/NUR";
+      url = "github:nix-community/NUR/156e6d0e026c1a7687a74c908ee0ccf0df796f72";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-25.11";
+      url = "github:nix-community/nixvim/64d9e2616f4ee2acee380d61ccf1f3d610e7e969";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     apple-silicon = {
-      url = "github:nix-community/nixos-apple-silicon";
+      url = "github:nix-community/nixos-apple-silicon/73b7103c4e3996e3e20868d510b0e8797f279323";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
