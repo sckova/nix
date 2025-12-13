c298ff5f0908fc20afaf64d4b75a7265ca7c3403
Sean Kovacs <kovacsmillio@gmail.com>
2025-12-13 12:09:17 -0500

    updates to tiling

--- home/tiling/waybar/config/scripts/clock.sh
+++ home/tiling/waybar/config/scripts/clock.sh
@@ -1,1 +1,13 @@
-date +"%a, %b %d @ %I:%M%P"
+#!/usr/bin/env bash
+
+day=$(date +%-d)
+
+case "$day" in
+    11|12|13) suffix="th" ;;
+    *1)       suffix="st" ;;
+    *2)       suffix="nd" ;;
+    *3)       suffix="rd" ;;
+    *)        suffix="th" ;;
+esac
+
+date +"%a, %b $day$suffix %Y @ %I:%M%P"
