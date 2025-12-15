0bc6837ba6d96d3366210e1e45b27d3322023e11
Sean Kovacs <kovacsmillio@gmail.com>
2025-12-15 16:15:20 -0500

    update kitty

--- home/terminal/kitty/default.nix
+++ home/terminal/kitty/default.nix
@@ -1,18 +1,42 @@
-{ config, lib, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  catppuccin-kitty = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "kitty";
+    rev = "b14e8385c827f2d41660b71c7fec1e92bdcf2676";
+    sha256 = "sha256-59ON7CzVgfZUo7F81qQZQ1r6kpcjR3OPvTl99gzDP8E=";
+  };
+
+  mergedConfig = pkgs.runCommand "mergedConfig" { } ''
+    mkdir -p $out
+    ${pkgs.gnused}/bin/sed 's/#cba6f7/${
+      pkgs.catppuccin.${config.catppuccin.flavor}.${config.catppuccin.accent}
+    }/g' ${catppuccin-kitty}/themes/${config.catppuccin.flavor}.conf > \
+    $out/${config.catppuccinUpper.flavor}${config.catppuccinUpper.accent}.conf
+  '';
+in
+{
+  home.file.".config/kitty/themes" = {
+    source = mergedConfig;
+    recursive = true;
+  };
+
   programs.kitty = {
     enable = true;
     enableGitIntegration = true;
-    themeFile = "Catppuccin-${config.catppuccinUpper.flavor}";
     font = {
       name = config.userOptions.fontMono.name;
       size = config.userOptions.fontMono.size;
     };
-    shellIntegration = {
-      enableFishIntegration = true;
-    };
+    shellIntegration.enableFishIntegration = true;
     settings = {
+      include = "/home/${config.userOptions.username}/.config/kitty/themes/${config.catppuccinUpper.flavor}${config.catppuccinUpper.accent}.conf";
       scrollback_lines = 10000;
       enable_audio_bell = false;
       update_check_interval = 0;
