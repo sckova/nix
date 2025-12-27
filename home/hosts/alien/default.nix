082437654cff6dff7c16cf930d54060303828e57
Sean Kovacs <kovacsmillio@gmail.com>
2025-12-27 08:25:33 -0500

    alien: remove games

--- home/hosts/alien/default.nix
+++ home/hosts/alien/default.nix
@@ -3,7 +3,8 @@
   pkgs,
   spicetify-nix,
   ...
-}: {
+}:
+{
   catppuccin = {
     accent = "blue";
     flavor = "mocha";
@@ -12,10 +13,6 @@
   home.packages = with pkgs; [
     # steam gtk theming
     adwsteamgtk
-
-    daggerfall-unity
-    shipwright
-    _2ship2harkinian
   ];
 
   programs.noctalia-shell.settings.brightness = {
@@ -33,28 +30,30 @@
     scale = 1.75;
   };
 
-  programs.spicetify = let
-    spicePkgs = spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
-  in {
-    enable = true;
+  programs.spicetify =
+    let
+      spicePkgs = spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+    in
+    {
+      enable = true;
 
-    enabledExtensions = with spicePkgs.extensions; [
-      adblock
-      hidePodcasts
-      shuffle # shuffle+ (special characters are sanitized out of extension names)
-    ];
-    enabledCustomApps = with spicePkgs.apps; [
-      newReleases
-      ncsVisualizer
-    ];
-    enabledSnippets = with spicePkgs.snippets; [
-      rotatingCoverart
-      pointer
-    ];
+      enabledExtensions = with spicePkgs.extensions; [
+        adblock
+        hidePodcasts
+        shuffle # shuffle+ (special characters are sanitized out of extension names)
+      ];
+      enabledCustomApps = with spicePkgs.apps; [
+        newReleases
+        ncsVisualizer
+      ];
+      enabledSnippets = with spicePkgs.snippets; [
+        rotatingCoverart
+        pointer
+      ];
 
-    theme = spicePkgs.themes.catppuccin;
-    colorScheme = config.catppuccin.flavor;
-  };
+      theme = spicePkgs.themes.catppuccin;
+      colorScheme = config.catppuccin.flavor;
+    };
 
   programs.plasma = {
     panels = [
