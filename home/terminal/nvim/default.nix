5010b6872e1a89e80b0ddf2913d72ad11439b2b3
Sean Kovacs <kovacsmillio@gmail.com>
2026-01-08 14:59:26 -0500

    pin to 25.11

--- home/terminal/nvim/default.nix
+++ home/terminal/nvim/default.nix
@@ -128,26 +128,6 @@
             }
           ];
         };
-        window = {
-          completion = {
-            border = "solid";
-          };
-          documentation = {
-            border = "solid";
-          };
-        };
-
-        mapping = {
-          "<C-Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-          "<C-j>" = "cmp.mapping.select_next_item()";
-          "<C-k>" = "cmp.mapping.select_prev_item()";
-          "<C-e>" = "cmp.mapping.abort()";
-          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<C-CR>" = "cmp.mapping.confirm({ select = true })";
-          "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
-        };
       };
       conform-nvim = {
         enable = true;
