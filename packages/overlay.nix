fbfb8f120cb47fff2a3530b72fe5fdd6363dbf11
Sean Kovacs <kovacsmillio@gmail.com>
2026-01-14 11:50:02 -0500

    asahi: switch to fairydust kernel, alien: switch to vanilla kernel

--- packages/overlay.nix
+++ packages/overlay.nix
@@ -1,3 +1,4 @@
 final: prev: {
   spotify-webapp = prev.callPackage (builtins.path {path = ./spotify/package.nix;}) {};
+  linux-asahi = prev.callPackage (builtins.path {path = ./linux-fairydust/package.nix;}) {};
 }
