fd29f84861f3a111cd0c2ee36729b91c6cf2c33c
Sean Kovacs <kovacsmillio@gmail.com>
2026-01-15 17:16:55 -0500

    switch to asahi kernel overlay

--- packages/overlay.nix
+++ packages/overlay.nix
@@ -1,5 +1,20 @@
 final: prev: {
   spotify-webapp = final.callPackage ./spotify-webapp {};
-  linux-asahi = final.callPackage ./linux-asahi {};
   altserver-linux = final.callPackage ./altserver {};
+
+  linuxPackages_asahi = prev.linuxPackages_asahi.override {
+    _kernelPatches = [
+      {
+        name = "Mailbox and RTKIT support";
+        patch = null;
+        structuredExtraConfig = with prev.lib.kernel; {
+          APPLE_MAILBOX = yes;
+          APPLE_RTKIT = yes;
+          APPLE_RTKIT_HELPER = yes;
+          RUST_APPLE_RTKIT = yes;
+          RUST_FW_LOADER_ABSTRACTIONS = yes;
+        };
+      }
+    ];
+  };
 }
