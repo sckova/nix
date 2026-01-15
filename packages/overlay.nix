614c87cc0c261d4bd9fd864c60c673766db8017b
Sean Kovacs <kovacsmillio@gmail.com>
2026-01-15 12:46:01 -0500

    add custom altserver package

--- packages/overlay.nix
+++ packages/overlay.nix
@@ -1,4 +1,5 @@
 final: prev: {
   spotify-webapp = final.callPackage ./spotify-webapp {};
   linux-asahi = final.callPackage ./linux-asahi {};
+  altserver-linux = final.callPackage ./altserver {};
 }
