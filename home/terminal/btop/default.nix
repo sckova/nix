431ff322405e139994a70fa9e7d594782b2e2e7a
Sean Kovacs <kovacsmillio@gmail.com>
2026-01-15 22:39:47 -0500

    move all url fetches into main flake

--- home/terminal/btop/default.nix
+++ home/terminal/btop/default.nix
@@ -3,19 +3,12 @@
   pkgs,
   ...
 }: let
-  catppuccin-btop = pkgs.fetchFromGitHub {
-    owner = "catppuccin";
-    repo = "btop";
-    rev = "f437574b600f1c6d932627050b15ff5153b58fa3";
-    sha256 = "sha256-mEGZwScVPWGu+Vbtddc/sJ+mNdD2kKienGZVUcTSl+c=";
-  };
-
   mergedConfig = pkgs.runCommand "mergedConfig" {} ''
     mkdir -p $out/themes
     ${pkgs.gnused}/bin/sed 's/blankFlavor/${config.catppuccin.flavor}/g' \
       ${./btop.conf} > $out/btop.conf
-    cp ${catppuccin-btop}/themes/catppuccin_latte.theme $out/themes/
-    cp ${catppuccin-btop}/themes/catppuccin_${config.catppuccin.flavor}.theme $out/themes/nixos.theme
+    cp ${pkgs.catppuccin-btop-git.src}/themes/catppuccin_latte.theme $out/themes/
+    cp ${pkgs.catppuccin-btop-git.src}/themes/catppuccin_${config.catppuccin.flavor}.theme $out/themes/nixos.theme
   '';
 in {
   home.file.".config/btop" = {
