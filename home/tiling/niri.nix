72fb6b27a98ac6d4f33628c9a2f019230aa44146
Sean Kovacs <kovacsmillio@gmail.com>
2026-02-07 13:21:53 -0500

    fix xwayland in niri

--- home/tiling/niri.nix
+++ home/tiling/niri.nix
@@ -13,7 +13,7 @@
       environment = {
         QT_QPA_PLATFORM = "wayland";
         QT_QPA_PLATFORMTHEME = "qt6ct";
-        DISPLAY = null;
+        DISPLAY = ":0";
         XCURSOR_THEME = config.userOptions.cursor.name;
         XCURSOR_SIZE = toString config.userOptions.cursor.size;
       };
