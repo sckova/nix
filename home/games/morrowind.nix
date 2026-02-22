15d6d34ef753c3162b3b384304edce1dcebd09aa
Sean Kovacs <kovacsmillio@gmail.com>
2026-02-21 20:00:16 -0500

    refactor session vars

--- home/games/morrowind.nix
+++ home/games/morrowind.nix
@@ -1,15 +1,10 @@
 {
-  config,
   pkgs,
   ...
 }:
 {
-  home = {
-    packages = with pkgs; [ openmw ];
-    sessionVariables = {
-      SDL_VIDEO_DRIVER = "wayland";
-    };
-  };
+  home.packages = with pkgs; [ openmw ];
+  home.sessionVariables.SDL_VIDEO_DRIVER = "wayland";
 
   programs.firefox.profiles.default.search.engines.uesp = {
     name = "Unofficial Elder Scrolls Pages";
