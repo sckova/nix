3dd29d1746a23aed46302ed6e365f57a1840357e
Sean Kovacs <kovacsmillio@gmail.com>
2026-03-03 18:05:01 -0500

    switch to swaylock-effects

--- home/tiling/default.nix
+++ home/tiling/default.nix
@@ -49,46 +49,46 @@
 
   programs.swaylock = with config.scheme; {
     enable = true;
-    package = pkgs.swaylock;
+    package = pkgs.swaylock-effects;
     settings = {
       # this would sometimes load the previous day's wallpaper
       # when it is run before the bing retrieval script finishes
-      # image = "~/.local/share/wallpaper/daily-colored.jpg";
-      # effect-blur = "7x5";
-      color = "000000"; # black
+      image = "~/.local/share/wallpaper/daily-colored.jpg";
+      effect-blur = "7x5";
+      # color = "000000"; # black
       font-size = 24;
       indicator-idle-visible = true;
       indicator-radius = 100;
       show-failed-attempts = true;
 
-      bs-hl-color = base09; # peach
-      caps-lock-bs-hl-color = base09; # peach
-      caps-lock-key-hl-color = base0E; # mauve
-      inside-color = base00; # base
-      inside-clear-color = base00; # base
-      inside-caps-lock-color = base00; # base
-      inside-ver-color = base00; # base
-      inside-wrong-color = base00; # base
-      key-hl-color = base0D; # blue
-      layout-bg-color = base00; # base
-      layout-border-color = base00; # base
-      layout-text-color = base05; # text
-      line-color = base00; # base
-      line-clear-color = base00; # base
-      line-caps-lock-color = base00; # base
-      line-ver-color = base00; # base
-      line-wrong-color = base00; # base
-      ring-color = base00; # base
-      ring-clear-color = base09; # peach
-      ring-caps-lock-color = base00; # base
-      ring-ver-color = base0B; # green
-      ring-wrong-color = base00; # base
+      bs-hl-color = base09 + "E6"; # peach
+      caps-lock-bs-hl-color = base09 + "E6"; # peach
+      caps-lock-key-hl-color = base0E + "E6"; # mauve
+      inside-color = base00 + "E6"; # base
+      inside-clear-color = base00 + "E6"; # base
+      inside-caps-lock-color = base00 + "E6"; # base
+      inside-ver-color = base00 + "E6"; # base
+      inside-wrong-color = base00 + "E6"; # base
+      key-hl-color = base0D + "E6"; # blue
+      layout-bg-color = base00 + "E6"; # base
+      layout-border-color = base00 + "E6"; # base
+      layout-text-color = base05 + "E6"; # text
+      line-color = base00 + "E6"; # base
+      line-clear-color = base00 + "E6"; # base
+      line-caps-lock-color = base00 + "E6"; # base
+      line-ver-color = base00 + "E6"; # base
+      line-wrong-color = base00 + "E6"; # base
+      ring-color = base00 + "E6"; # base
+      ring-clear-color = base09 + "E6"; # peach
+      ring-caps-lock-color = base00 + "E6"; # base
+      ring-ver-color = base0B + "E6"; # green
+      ring-wrong-color = base00 + "E6"; # base
       separator-color = "00000000"; # transparent
-      text-color = base05; # text
-      text-clear-color = base09; # peach
-      text-caps-lock-color = base0E; # mauve
-      text-ver-color = base05; # text
-      text-wrong-color = base08; # red
+      text-color = base05 + "E6"; # text
+      text-clear-color = base09 + "E6"; # peach
+      text-caps-lock-color = base0E + "E6"; # mauve
+      text-ver-color = base05 + "E6"; # text
+      text-wrong-color = base08 + "E6"; # red
     };
   };
 
@@ -101,7 +101,7 @@
     };
 
     Service = {
-      ExecStart = "${pkgs.swaylock}/bin/swaylock";
+      ExecStart = "${config.programs.swaylock.package}/bin/swaylock";
       Restart = "on-failure";
     };
 
