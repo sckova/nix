dbc99ec13258e9b02d81d642e09d455e1ef99efb
Sean Kovacs <kovacsmillio@gmail.com>
2026-03-26 11:25:07 -0400

    alien: configure proton vars and add steam big picture entry

--- home/hosts/alien/default.nix
+++ home/hosts/alien/default.nix
@@ -1,8 +1,5 @@
 {
-  config,
   pkgs,
-  lib,
-  pkgs-unstable,
   ...
 }:
 {
@@ -12,14 +9,34 @@
   };
 
   home.packages = with pkgs; [
-    pkgs-unstable.ckan
-
+    ckan
     spotify
     adwsteamgtk
     daggerfall-unity
     vintagestory
     gamemode
   ];
+
+  # PROTON_ENABLE_NVAPI=1 PROTON_DLSS_UPGRADE=1 PROTON_ENABLE_NGX_UPDATER=1  DXVK_NVAPI_DRS_SETTINGS=NGX_DLSS_SR_MODE=balanced MANGOHUD_CONFIG="fps_limit=144,gamemode,ram,vram" mangohud gamemoderun %command%
+  xdg.desktopEntries.steam-big-picture = {
+    name = "Steam (Big Picture)";
+    icon = "steam";
+    exec = "gamescope -e --force-grab-cursor -s 2 -- steam -tenfoot";
+    terminal = false;
+    categories = [
+      "Network"
+      "FileTransfer"
+      "Game"
+    ];
+  };
+
+  home.sessionVariables = {
+    PROTON_ENABLE_NVAPI = 1;
+    PROTON_DLSS_UPGRADE = 1;
+    PROTON_ENABLE_NGX_UPDATER = 1;
+    DXVK_NVAPI_DRS_SETTINGS = "NGX_DLSS_SR_MODE=balanced";
+    MANGOHUD_CONFIG = "fps_limit=144,gamemode,ram,vram";
+  };
 
   programs.noctalia-shell.settings.brightness.enableDdcSupport = true;
   programs.noctalia-shell.settings.bar = {
