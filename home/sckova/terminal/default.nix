b7f829b859ebb4bfc5c256e2338f6b2fc888213a
Sean Kovacs <kovacsmillio@gmail.com>
2026-04-09 10:00:49 -0400

    configure fzf

--- home/sckova/terminal/default.nix
+++ home/sckova/terminal/default.nix
@@ -1,7 +1,6 @@
 {
   config,
   pkgs,
-  lib,
   ...
 }:
 {
@@ -20,7 +19,6 @@
     wget
     ripgrep
     ncdu
-    fzf
     wl-clipboard
     rclone
     waypipe
@@ -78,5 +76,30 @@
       enableFishIntegration = true;
     };
     lazysql.enable = true;
+    fzf = {
+      enable = true;
+      enableFishIntegration = true;
+      defaultOptions = [
+        "--height 40%"
+        "--border"
+      ];
+      colors = with config.scheme.withHashtag; {
+        bg = base00;
+        "bg+" = base01;
+        border = base03;
+        fg = base05;
+        "fg+" = base05;
+        header = base08;
+        hl = base08;
+        "hl+" = base08;
+        info = config.scheme.withHashtag.${config.colors.accent};
+        label = base05;
+        marker = base07;
+        pointer = base06;
+        prompt = config.scheme.withHashtag.${config.colors.accent};
+        "selected-bg" = base02;
+        spinner = base06;
+      };
+    };
   };
 }
