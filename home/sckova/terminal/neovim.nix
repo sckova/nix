7d08abb7addd6788be60682ddbc795440bb3adaa
Sean Kovacs <kovacsmillio@gmail.com>
2026-04-09 11:54:43 -0400

    neovim: swap accent with whatever color it was before

--- home/sckova/terminal/neovim.nix
+++ home/sckova/terminal/neovim.nix
@@ -28,24 +28,34 @@
     colorschemes.base16 = {
       enable = true;
       setUpBar = false;
-      colorscheme = with config.scheme.withHashtag; {
-        base00 = base00;
-        base01 = base01;
-        base02 = base02;
-        base03 = base03;
-        base04 = base04;
-        base05 = base05;
-        base06 = base06;
-        base07 = base07;
-        base08 = config.scheme.withHashtag.${config.colors.accent};
-        base09 = base09;
-        base0A = base0A;
-        base0B = base0B;
-        base0C = base0C;
-        base0D = base0D;
-        base0E = base0E;
-        base0F = base0F;
-      };
+      colorscheme =
+        let
+          cleanScheme = {
+            inherit (config.scheme.withHashtag)
+              base00
+              base01
+              base02
+              base03
+              base04
+              base05
+              base06
+              base07
+              base08
+              base09
+              base0A
+              base0B
+              base0C
+              base0D
+              base0E
+              base0F
+              ;
+          };
+        in
+        cleanScheme
+        // {
+          base08 = cleanScheme.${config.colors.accent};
+          ${config.colors.accent} = cleanScheme.base08;
+        };
     };
 
     keymaps = [
