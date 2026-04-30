83a46b39000598d5a0648f59dbac5dca99d94e8f
Sean Kovacs <kovacsmillio@gmail.com>
2026-04-30 09:57:45 -0400

    niri: move outputs to respective hosts

--- home/sckova/hosts/peach/default.nix
+++ home/sckova/hosts/peach/default.nix
@@ -24,16 +24,30 @@
       density = "spacious";
     };
 
-    niri.settings.outputs."eDP-1" = {
-      scale = 1.5;
-      mode = {
-        width = 3024;
-        height = 1964;
-        refresh = 120.000;
+    niri.settings.outputs = {
+      "eDP-1" = {
+        scale = 1.5;
+        mode = {
+          width = 3024;
+          height = 1964;
+          refresh = 120.000;
+        };
+        position = {
+          x = 272;
+          y = 1440;
+        };
       };
-      position = {
-        x = 272;
-        y = 1440;
+      "HDMI-A-1" = {
+        mode = {
+          width = 3840;
+          height = 2160;
+          refresh = 144.000;
+        };
+        scale = 1.5;
+        position = {
+          x = 0;
+          y = 0;
+        };
       };
     };
   };
