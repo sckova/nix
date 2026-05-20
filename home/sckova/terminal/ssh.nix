f163c103dcfbaa77fe06f68e82e9c97f8a76b883
Sean Kovacs <kovacsmillio@gmail.com>
2026-05-20 18:56:12 -0400

    ssh: further configure

--- home/sckova/terminal/ssh.nix
+++ home/sckova/terminal/ssh.nix
@@ -1,17 +1,31 @@
 {
-  programs.ssh.settings = {
-    "ovips.us.to" = {
-      HostName = "ovips.us.to";
-      User = "git";
-      Port = 222;
-    };
-    "oracle" = {
-      HostName = "vips.taila30609.ts.net";
-      User = "ubuntu";
-    };
-    "*" = {
-      StrictHostKeyChecking = "no";
-      IdentityFile = "~/.ssh/key";
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false; # this will be gone in the future
+    settings = {
+      "ovips.us.to" = {
+        HostName = "ovips.us.to";
+        User = "git";
+        Port = 222;
+      };
+      "oracle" = {
+        HostName = "vips.taila30609.ts.net";
+        User = "ubuntu";
+      };
+      "*" = {
+        StrictHostKeyChecking = "no";
+        IdentityFile = "~/.ssh/key";
+        ForwardAgent = false;
+        AddKeysToAgent = "no";
+        Compression = false;
+        ServerAliveInterval = 0;
+        ServerAliveCountMax = 3;
+        HashKnownHosts = false;
+        UserKnownHostsFile = "~/.ssh/known_hosts";
+        ControlMaster = "no";
+        ControlPath = "~/.ssh/master-%r@%n:%p";
+        ControlPersist = "no";
+      };
     };
   };
 }
