9b1b17813cbe7b0c1dfe8ea0b4a49e958b8865fc
Sean Kovacs <kovacsmillio@gmail.com>
2026-05-19 21:16:55 -0400

    re-enable autologin

--- system/services/default.nix
+++ system/services/default.nix
@@ -11,7 +11,7 @@
 
   services = {
     displayManager = {
-      autoLogin.enable = false;
+      autoLogin.enable = true;
       autoLogin.user = "sckova";
       defaultSession = "niri";
       gdm.enable = true;
