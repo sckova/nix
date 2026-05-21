ca1d278138441c10d098eb280823a704e3a4c734
Sean Kovacs <kovacsmillio@gmail.com>
2026-05-21 10:58:11 -0400

    fish: remove fish_functions directory

--- home/sckova/terminal/fish.nix
+++ home/sckova/terminal/fish.nix
@@ -24,11 +24,43 @@
     functions = {
       fish_prompt = {
         description = "Write out the prompt";
-        body = builtins.readFile ./fish_functions/fish-prompt.fish;
+        body = /* fish */ ''
+          source ~/.config/fish/colors.fish
+          set -l last_status $status
+          set -l nix_shell_info (
+            if test -n "$IN_NIX_SHELL"
+              echo -n "<nix-shell> "
+            end
+          )
+          set -g color_user $color_base0C
+          set -g color_host $color_base08
+          set -g color_cwd $color_base0B
+
+          # Set host color based on hostname
+          if test (hostname) = peach
+              set -g color_host $color_accent
+          else if test (hostname) = alien
+              set -g color_host $color_accent
+          else if test (uname) = Darwin
+              set -g color_host $color_base0A
+          end
+
+          set -l user_host (set_color $color_user)"$USER"(set_color normal)"@"(set_color $color_host)(prompt_hostname)(set_color normal)
+          set -l cwd (set_color $color_cwd)(prompt_pwd)(set_color normal)
+          set -l git_info (fish_git_prompt)
+          echo -s "$user_host" " " "$cwd" "$git_info $nix_shell_info"
+          echo -n -s "> "
+        '';
       };
       nix-shell = {
         description = "Wrapper for nix-shell that runs fish by default";
-        body = builtins.readFile ./fish_functions/nix-shell.fish;
+        body = /* fish */ ''
+          if test (count $argv) -eq 0
+              command nix-shell --run fish
+          else
+              command nix-shell --run fish $argv
+          end
+        '';
       };
     };
   };
--- home/sckova/terminal/fish_functions/nix-shell.fish
+++
@@ -1,5 +0,0 @@
-if test (count $argv) -eq 0
-  command nix-shell --run fish
-else
-  command nix-shell --run fish $argv
-end
