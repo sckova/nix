f22680c181efcf17b833abaf91db921d7fda43b6
Sean Kovacs <kovacsmillio@gmail.com>
2026-07-31 15:52:50 -0400

    remove garnix substitutor

--- lib/nix-settings.nix
+++ lib/nix-settings.nix
@@ -23,14 +23,12 @@
 
       substituters = [
         "https://attic.xuyh0120.win/lantian"
-        "https://cache.garnix.io"
         "https://nixos-apple-silicon.cachix.org"
         "https://noctalia.cachix.org"
       ];
 
       trusted-public-keys = [
         "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
         "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       ];
