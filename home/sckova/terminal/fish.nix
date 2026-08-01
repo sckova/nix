35e7848022165a1b2ab06a2d6fd3b6f47ef4cba1
Sean Kovacs <kovacsmillio@gmail.com>
2026-08-01 08:46:36 -0400

    fish: remove host-specific prompt coloring

--- home/sckova/terminal/fish.nix
+++ home/sckova/terminal/fish.nix
@@ -39,21 +39,12 @@
             set -l last_status $status
             set -l nix_shell_info (
               if test -n "$IN_NIX_SHELL"
-                echo -n "<nix-shell> "
+              echo -n "<nix-shell> "
               end
             )
             set -g color_user $color_base0C
-            set -g color_host $color_base08
+            set -g color_host $color_accent
             set -g color_cwd $color_base0B
-
-            # Set host color based on hostname
-            if test (hostname) = peach
-                set -g color_host $color_accent
-            else if test (hostname) = alien
-                set -g color_host $color_accent
-            else if test (hostname) = skmbp
-                set -g color_host $color_accent
-            end
 
             set -l user_host (set_color $color_user)"$USER"(set_color normal)"@"(set_color $color_host)(prompt_hostname)(set_color normal)
             set -l cwd (set_color $color_cwd)(prompt_pwd)(set_color normal)
@@ -136,7 +127,7 @@
       initContent = /* zsh */ ''
         if [[ $(ps -o command= -p "$PPID" | awk '{print $1}') != 'fish' ]]
         then
-            exec fish -l
+          exec fish -l
         fi
       '';
     };
