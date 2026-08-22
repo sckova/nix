03e4c9ee1309cc8aaf040db0bc2b97f76be7d242
Sean Kovacs <kovacsmillio@gmail.com>
2026-08-22 15:58:15 -0400

    paneru: configure keybinds to match niri

--- home/sckova/tiling/paneru.nix
+++ home/sckova/tiling/paneru.nix
@@ -13,7 +13,7 @@
 
     settings = {
       options = {
-        animation_speed = 14;
+        animation_speed = 15;
         focus_follows_mouse = true;
         mouse_follows_focus = false;
 
@@ -24,13 +24,50 @@
         ];
       };
 
-      bindings = {
-        quit = "ctrl + alt - q";
-        window_center = "alt - c";
-        window_focus_east = "cmd - rightarrow";
-        window_focus_west = "cmd - leftarrow";
-        window_resize = "cmd - r";
-      };
+      bindings =
+        let
+          mod = "alt";
+        in
+        {
+          # Lifecycle
+          quit = "${mod} + shift - e";
+        }
+        // {
+          # Focus Navigation
+          window_focus_east = "${mod} - rightarrow";
+          window_focus_first = "${mod} - home";
+          window_focus_last = "${mod} - end";
+          window_focus_north = "${mod} - uparrow";
+          window_focus_south = "${mod} - downarrow";
+          window_focus_unmanaged = "${mod} + shift - v";
+          window_focus_west = "${mod} - leftarrow";
+        }
+        // {
+          # Window & Column Movement
+          window_swap_east = "${mod} + ctrl - rightarrow";
+          window_swap_first = "${mod} + ctrl - home";
+          window_swap_last = "${mod} + ctrl - end";
+          window_swap_north = "${mod} + ctrl - uparrow";
+          window_swap_south = "${mod} + ctrl - downarrow";
+          window_swap_west = "${mod} + ctrl - leftarrow";
+        }
+        // {
+          # Layout & Sizing
+          window_center = "${mod} - c";
+          window_equalize = "${mod} + ctrl - r";
+          window_fullwidth = "${mod} - f";
+          window_resize = "${mod} - r";
+          window_shrink = "${mod} + shift - r";
+        }
+        // {
+          # Column Stacking
+          window_stack = "${mod} - leftbracket";
+          window_unstack = "${mod} - rightbracket";
+        }
+        // {
+          # Floating & Management
+          window_manage = "${mod} - v";
+        };
 
       decorations.active.border = {
         color = config.scheme.withHashtag.${config.colors.accent};
@@ -46,11 +83,16 @@
         top = 8;
       };
 
+      swipe = {
+        fingers_count = 3; # swipe module is enabled when this >=3
+      };
+
       windows.all = {
         bundle_id = ".*";
-        title = ".*";
         # the following doesn't work as expected (padding the inner areas)
-        # horizontal_padding = 8;
+        horizontal_padding = 8;
+        title = ".*";
+        vertical_padding = 0;
         width = 0.5;
       };
     };
