cff8f1b9b5741d16deecd2f5b06891b39318675e
Sean Kovacs <kovacsmillio@gmail.com>
2026-08-23 15:31:30 -0400

    openmw: add performance app

--- home/sckova/games/morrowind.nix
+++ home/sckova/games/morrowind.nix
@@ -1,5 +1,11 @@
 # home/sckova/games/morrowind.nix
-{ pkgs-unstable, ... }: {
+{
+  pkgs,
+  hostname,
+  pkgs-unstable,
+  ...
+}:
+{
   home = {
     packages = with pkgs-unstable; [ openmw ];
 
@@ -19,4 +25,47 @@
       }
     ];
   };
+
+  xdg.desktopEntries.openmw-gamescope =
+    let
+      isAlien = hostname == "alien";
+
+      openmw-launcher =
+        pkgs.writeShellScriptBin "openmw-launcher" # bash
+          (
+            if isAlien then
+              /* bash */ ''
+                exec ${pkgs.gamescope}/bin/gamescope \
+                  --force-grab-cursor \
+                  --cursor-scale-height 1400 \
+                  -s 3 \
+                  -r 144 \
+                  -f \
+                  -- \
+                  env \
+                  __NV_PRIME_RENDER_OFFLOAD=1 \
+                  __GLX_VENDOR_LIBRARY_NAME=nvidia \
+                  SDL_VIDEODRIVER=x11 \
+                  ${pkgs.gamemode}/bin/gamemoderun ${pkgs.openmw}/bin/openmw "$@"
+              ''
+            else
+              /* bash */ ''
+                exec ${pkgs.gamemode}/bin/gamemoderun ${pkgs.openmw}/bin/openmw "$@"
+              ''
+          );
+    in
+    {
+      categories = [
+        "Game"
+        "RolePlaying"
+      ];
+
+      comment = "OpenMW Wrapper Launcher";
+      exec = "${openmw-launcher}/bin/openmw-launcher";
+      genericName = "Role-Playing Game";
+      icon = "openmw";
+      name = "OpenMW (performance)";
+      settings.Keywords = "openmw;morrowind;";
+      terminal = false;
+    };
 }
