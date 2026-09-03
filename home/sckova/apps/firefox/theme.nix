8dd515a0f1adc77dd4483d1b48d2028bc1500fee
Sean Kovacs <kovacsmillio@gmail.com>
2026-09-03 17:52:11 -0400

    firefox-gnome: configure colors

--- home/sckova/apps/firefox/theme.nix
+++ home/sckova/apps/firefox/theme.nix
@@ -26,33 +26,137 @@ let
 in
 {
   home.file = {
+    "${firefoxUserChromePath}/colors.css".text = with config.scheme.withHashtag; /* css */ ''
+      @namespace xul url("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul");
+      @media (prefers-color-scheme: dark) {
+        :root {
+          /* Palette */
+          --gnome-palette-blue-1: ${base0D} !important;
+          --gnome-palette-blue-2: ${base0D} !important;
+          --gnome-palette-blue-3: ${base0D} !important;
+          --gnome-palette-blue-4: ${base0D} !important;
+          --gnome-palette-blue-5: ${base0D} !important;
+          --gnome-palette-green-1: ${base0B} !important;
+          --gnome-palette-green-2: ${base0B} !important;
+          --gnome-palette-green-3: ${base0B} !important;
+          --gnome-palette-green-4: ${base0B} !important;
+          --gnome-palette-green-5: ${base0B} !important;
+          --gnome-palette-yellow-1: ${base0A} !important;
+          --gnome-palette-yellow-2: ${base0A} !important;
+          --gnome-palette-yellow-3: ${base0A} !important;
+          --gnome-palette-yellow-4: ${base0A} !important;
+          --gnome-palette-yellow-5: ${base0A} !important;
+          --gnome-palette-orange-1: ${base09} !important;
+          --gnome-palette-orange-2: ${base09} !important;
+          --gnome-palette-orange-3: ${base09} !important;
+          --gnome-palette-orange-4: ${base09} !important;
+          --gnome-palette-orange-5: ${base09} !important;
+          --gnome-palette-red-1: ${base08} !important;
+          --gnome-palette-red-2: ${base08} !important;
+          --gnome-palette-red-3: ${base08} !important;
+          --gnome-palette-red-4: ${base08} !important;
+          --gnome-palette-red-5: ${base08} !important;
+          --gnome-palette-purple-1: ${base0E} !important;
+          --gnome-palette-purple-2: ${base0E} !important;
+          --gnome-palette-purple-3: ${base0E} !important;
+          --gnome-palette-purple-4: ${base0E} !important;
+          --gnome-palette-purple-5: ${base0E} !important;
+          --gnome-palette-brown-1: ${base0F} !important;
+          --gnome-palette-brown-2: ${base0F} !important;
+          --gnome-palette-brown-3: ${base0F} !important;
+          --gnome-palette-brown-4: ${base0F} !important;
+          --gnome-palette-brown-5: ${base0F} !important;
+          --gnome-palette-light-1: ${base05} !important;
+          --gnome-palette-light-2: ${base05} !important;
+          --gnome-palette-light-3: ${base05} !important;
+          --gnome-palette-light-4: ${base05} !important;
+          --gnome-palette-light-5: ${base05} !important;
+          --gnome-palette-dark-1: ${base00} !important;
+          --gnome-palette-dark-2: ${base00} !important;
+          --gnome-palette-dark-3: ${base00} !important;
+          --gnome-palette-dark-4: ${base00} !important;
+          --gnome-palette-dark-5: ${base00} !important;
+          /* Colors */
+          --gnome-standalone-color-oklab: max(l, 0.85) a b;
+          --gnome-destructive-bg: ${base08};
+          --gnome-success-bg: ${base0B};
+          --gnome-warning-bg: ${base0A};
+          --gnome-error-bg: ${base08};
+          --gnome-toolbar-star-button: var(--gnome-palette-yellow-1);
+          /* Window */
+          --gnome-window-background: ${base00};
+          --gnome-window-color: ${base05};
+          --gnome-view-background: ${base10};
+          --gnome-sidebar-background: ${base01};
+          --gnome-secondary-sidebar-background: ${base02};
+          /* Card */
+          --gnome-card-background: color-mix(in srgb, ${base06} 8%, transparent);
+          --gnome-card-shade-color: color-mix(in srgb, ${base00} 36%, transparent);
+          /* Menu */
+          --gnome-menu-background: ${base02};
+          /* Header bar */
+          --gnome-headerbar-background: color-mix(
+            in srgb,
+            ${base00} 90%,
+            transparent
+          ) !important;
+          --gnome-headerbar-shade-color: ${base00};
+          /* Tabs */
+          --gnome-tabbar-identity-color-blue: ${base16};
+          --gnome-tabbar-identity-color-green: var(--gnome-palette-green-1);
+          --gnome-tabbar-identity-color-yellow: var(--gnome-palette-yellow-2);
+          --gnome-tabbar-identity-color-orange: var(--gnome-palette-orange-3);
+          --gnome-tabbar-identity-color-red: var(--gnome-palette-red-1);
+          --gnome-tabbar-identity-color-purple: var(--gnome-palette-purple-1);
+          /* Miscellaneous */
+          --gnome-shade-color: color-mix(in srgb, ${base00} 25%, transparent);
+          /* Text color for Firefox Logo in new private tab */
+          --gnome-private-wordmark: ${base07};
+          /* New private tab background */
+          --gnome-private-in-content-page-background: ${base00};
+          /* Private browsing info box */
+          --gnome-private-text-primary-color: ${base07};
+          /* Backdrop colors */
+          &:-moz-window-inactive {
+            --gnome-sidebar-background: ${base10};
+            --gnome-secondary-sidebar-background: ${base11};
+          }
+          /* Private browsing colors */
+          &[privatebrowsingmode="temporary"] {
+            /* Headerbar */
+            --gnome-headerbar-background: color-mix(
+              in srgb,
+              ${base0E} 20%,
+              ${base01}
+            ) !important;
+            &:-moz-window-inactive {
+              --gnome-headerbar-background: color-mix(
+                in srgb,
+                ${base00} 90%,
+                transparent
+              ) !important;
+            }
+          }
+        }
+      }
+    '';
+
     "${firefoxUserChromePath}/firefox-gnome-theme" = {
       recursive = true;
       source = inputs.firefox-gnome-theme;
     };
 
-    "${firefoxUserChromePath}/userChrome.css".text =
-      /* css */ ''
-        @import "firefox-gnome-theme/userChrome.css";
-      ''
-      + lib.optionalString isLinux /* css */ ''
-        #toolbar-menubar {
-          display: none !important;
-        }
+    "${firefoxUserChromePath}/userChrome.css".text = /* css */ ''
+      @import "firefox-gnome-theme/userChrome.css";
+      @import "colors.css";
+      #toolbar-menubar {
+        display: none !important;
+      }
 
-        #menubar-items {
-          visibility: hidden !important;
-        }
-
-        /* apply 90% transparency to the frame */
-        html > body {
-          background: color-mix(
-            in srgb,
-            var(--toolbox-background-color) 90%,
-            transparent
-          ) !important;
-        }
-      '';
+      #menubar-items {
+        visibility: hidden !important;
+      }
+    '';
 
     "${firefoxUserChromePath}/userContent.css".text = /* css */ ''
       @import "firefox-gnome-theme/userContent.css";
@@ -78,7 +182,7 @@ in
     # Allow dragging the window from headerbar buttons.
     dragWindowHeaderbarButtons = false;
     # Adaptive Tab Bar Colour support.
-    extensions.adaptiveTabBarColour = true;
+    extensions.adaptiveTabBarColour = false;
     # Hide the tab bar when only one tab is open.
     hideSingleTab = true;
     # Hide unified extensions button from the navbar.
